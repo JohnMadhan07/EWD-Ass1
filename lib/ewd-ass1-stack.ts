f6f34d15502ea939c62d05a9be0e9c327f46a910
Madhan <madhan12n@gmail.com>
2024-03-08 10:27:07 +0000

    MovieReviews DynamoDb table created

--- lib/ewd-ass1-stack.ts
+++ lib/ewd-ass1-stack.ts
@@ -1,16 +1,18 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class EwdAss1Stack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'EwdAss1Queue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const movieReviewsTable = new dynamodb.Table(this, "MovieReviewsTable", {
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      partitionKey: { name: "id", type: dynamodb.AttributeType.NUMBER },
+      sortKey:{name:"ReviewDate", type:dynamodb.AttributeType.STRING},
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      tableName: "MovieReviews",
+    });
   }
 }
